db20ab453fa15fb00e4636c4c34691cb1d5592e7
Rutvik Manvar <rutvikmanvar99@gmail.com>
2026-02-26 22:24:39 +0530

    app.js push

---
+++ app.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('V1');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
